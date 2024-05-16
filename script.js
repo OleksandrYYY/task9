9428f2cd16e8ababeb312ba36a097ead9930af7c
Oleksandr Horbenko <oleksandr.horbenko.01@gmail.com>
2024-05-17 00:39:25 +0300

    Created new files index.html and script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+const elemWithId = document.querySelector("#headerTwo");
+console.log(elemWithId);
+const firstElemSection = document.querySelector("section");
+console.log(firstElemSection);
+const elemLi = document.querySelector("ul li:nth-child(5)");
+console.log(elemLi);
+const elemWithClass = document.querySelector(".hatredLevelBlock")
+console.log(elemWithClass);
